975609c4f8bc47714874ff24f289492c575cf5d9
Nastia <nastia7408@gmail.com>
2025-09-08 11:35:30 +0300

    add secondary

---
+++ src/app/core/theme/secondary/secondary.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-secondary',
+  standalone: false,
+  templateUrl: './secondary.component.html',
+  styleUrl: './secondary.component.scss'
+})
+export class SecondaryComponent {
+
+}
