827fd87b81b726022e7e67ec9c4b72337e876ecf
Nika <danzi6386@gmail.com>
2025-09-09 11:35:28 +0300

    fixed transaction create function

--- src/app/core/theme/public/public.component.ts
+++ src/app/core/theme/public/public.component.ts
@@ -1,7 +1,11 @@
-import { Component } from '@angular/core';
-import { UserService } from 'src/app/modules/user/services/user.service';
 import { Platform } from '@angular/cdk/platform';
-import { FormInterface } from 'src/app/core/modules/form/interfaces/form.interface';
+import { Component, inject } from '@angular/core';
+import { firstValueFrom } from 'rxjs';
+import { budgettransactionFormComponents } from 'src/app/modules/budgettransaction/formcomponents/budgettransaction.formcomponents';
+import { Budgettransaction } from 'src/app/modules/budgettransaction/interfaces/budgettransaction.interface';
+import { BudgettransactionService } from 'src/app/modules/budgettransaction/services/budgettransaction.service';
+import { UserService } from 'src/app/modules/user/services/user.service';
+import { FormService } from '../../modules/form/form.service';
 
 @Component({
 	selector: 'app-public',
@@ -54,19 +58,29 @@ export class PublicComponent {
 	onRangeChange() {
 		console.log('Вибрано період:', this.selectedRange);
 	}
-	protected create() {
-		this.__form.modal<Document>(this.form, {
+
+	private _formService = inject(FormService);
+
+	private _transactionService = inject(BudgettransactionService);
+
+	createTransaction() {
+		this._formService.modal<Document>(budgettransactionFormComponents, {
 			label: 'Create',
 			click: async (created: unknown, close: () => void) => {
 				close();
-				this.preCreate(created as Document);
 
 				await firstValueFrom(
-					this.crudService.create(created as Document)
+					this._transactionService.create(
+						created as Budgettransaction
+					)
 				);
 
 				this.setDocuments();
 			}
 		});
+	}
+
+	setDocuments() {
+		// update content of this page
 	}
 }
